7fd9dd46b50d9cbe721be705667a951c44354351
lorenzosharks <karsonx27@lakesideschool.org>
2025-05-09 00:11:03 -0700

    Updated README.md with additional project details and usage instructions

---
+++ extension.py
@@ -0,0 +1,9 @@
+"""
+Write a separate program that uses polynomials to approximate values of logarithms. For this 
+program, the core function will compute values of the natural log only for inputs of 1 to √2 and 
+other functions will use properties of logarithms to extend the allowed inputs.  
+ 
+Necessary constants: LN2 = np.log(2) and  SQRT2 = np.sqrt(2)
+
+"""
+
